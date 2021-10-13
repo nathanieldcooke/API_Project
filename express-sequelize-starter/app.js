f5d23129ff7e81f4702daf36da567bfa10610adc
nathanieldcooke <nathanield.cooke@gmail.com>
2021-10-13 08:44:31 -0700

    complete phase 4 front-end

--- express-sequelize-starter/app.js
+++ express-sequelize-starter/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors')
 const { environment } = require('./config');
 const app = express();
 const indexRouter = require('./routes/index')
 const tweetsRouter = require('./routes/tweets')
 
+app.use(cors({ origin: "http://localhost:4000" }));
 app.use(express.json())
 app.use(morgan("dev"));
-
 
 app.use('/', indexRouter);
 app.use('/tweets', tweetsRouter);
